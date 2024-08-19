7fef5819c5e829d5be1c4844f8435b450c93096f
Anthony Flores <64795591+Anthobetto@users.noreply.github.com>
2024-08-19 19:39:12 +0000

    fix: home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,14 @@
 import React from "react";
- Cards
 import { Navbar } from "./Navbar.jsx";
 import { Jumbotron } from "./Jumbotron.jsx";
-import { Navbar } from "./Navbar.jsx";
-
 import { Cards } from "./Cards.jsx";
 
 const Home = () => {
 	return (
 		<>
 			<Navbar />
-			<Cards />
- jumbotron
-
 			<Jumbotron/>
-
-			<Navbar/>
- develop
-
-develop
+			<Cards/>
 		</>
 	);
 };
